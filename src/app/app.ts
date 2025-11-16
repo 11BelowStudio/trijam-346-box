d928396e6e184a8afecc5c91c07d16c251b28f6c
11BelowStudio <60523642+11BelowStudio@users.noreply.github.com>
2025-11-16 17:52:24 +0000

    more sfx

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
-import { Component, signal } from '@angular/core';
+import {afterNextRender, Component, ElementRef, inject, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import {Asset_db} from '../game_backend/config/asset_db';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,24 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('trijam-346-box');
-}
 
+  ngOnInit(): void {
+    // Component initialization logic goes here
+    //Asset_db.sfx_game_over();
+  }
+  /*
+  constructor() {
+    const elementRef = inject(ElementRef);
+    afterNextRender({
+      read: (didWrite) => {
+        Asset_db.sfx_game_over();
+      }
+
+    });
+  }*/
+
+
+
+}
--- src/game_backend/config/asset_db.ts
+++ src/game_backend/config/asset_db.ts
@@ -4,36 +4,48 @@ import {Howl, Howler} from 'howler';
 
 import {sprintf} from 'sprintf-js';
 
-export const Asset_db = {
+const Asset_db = {
 
 
 
-  sfx_money: function(): void {
-    _random_itm(sfx_money_arr).play();
+  sfx_money: function(): [Howl, number] {
+    const snd: Howl = _random_itm(sfx_money_arr);
+    return [snd, snd.play()]
   },
 
-  sfx_game_over: function(): void {
-    _random_itm(sfx_gameover_arr).play();
+  sfx_game_over: function(): [Howl, number] {
+    const snd: Howl = _random_itm(sfx_gameover_arr);
+    return [snd, snd.play()];
   },
 
-  sfx_item: function(): void {
-    _random_itm(sfx_item_arr).play();
+  sfx_item: function(): [Howl, number] {
+    const snd: Howl = _random_itm(sfx_item_arr);
+    return [snd, snd.play()];
   },
 
-  sfx_gamba: function(): void {
-    _random_itm(sfx_gamba_arr).play();
+  sfx_gamba: function(): [Howl, number] {
+    const snd: Howl = _random_itm(sfx_gamba_arr);
+    return [snd, snd.play()];
   },
 
-  sfx_no: function(): void {
-    _random_itm(sfx_no_arr).play();
+  sfx_no: function(): [Howl, number]{
+    const snd: Howl = _random_itm(sfx_no_arr);
+    return [snd, snd.play()];
   },
 
-  sfx_sell: function(): void {
-    _random_itm(sfx_sell_arr).play();
+  sfx_sell: function(): [Howl, number] {
+    const snd: Howl = _random_itm(sfx_sell_arr);
+    return [snd, snd.play()];
   },
 
-  sfx_yes: function(): void {
-    _random_itm(sfx_yes_arr).play();
+  sfx_yes: function(): [Howl, number] {
+    const snd: Howl = _random_itm(sfx_yes_arr);
+    return [snd, snd.play()];
+  },
+
+  sfx_buy: function(): [Howl, number] {
+    const snd: Howl = _random_itm(sfx_purchase_arr);
+    return [snd, snd.play()];
   }
 }
 
@@ -50,7 +62,7 @@ function howl_loader(filename: string, count: number = 0) : Howl[] {
   let res: Howl[] = []
   for (let i = 1; i <= count ; i++) {
     res.push(new Howl({
-      src: [sprintf(filename, count)]
+      src: [sprintf(filename, i)]
     }))
   }
   return res;
@@ -62,13 +74,24 @@ const _sfx_the_word_playing: Howl = new Howl({
   src :["/audio/voice/playing.mp3"]
 });
 
-const _sfx_thank_you_for: Howl = new Howl({
+const _sfx_thank_you_for_playing: Howl = new Howl({
   src: ["/audio/voice/thank_you_for.mp3"],
   onend: soundId => {
     if (Math.random() >= 0.5){
-      _sfx_the_word_playing.play();
+      const h1: Howl = _sfx_the_word_playing;
+      const id1: number = h1.play();
+      if (Math.random() >= 0.5){
+        h1.once('end', () => {
+          sfx_come_again.play();
+        }, id1);
+      }
     } else {
-      Asset_db.sfx_money();
+      const [h2, id2] = Asset_db.sfx_money();
+      if (Math.random() >= 0.5){
+        h2.once('end', () => {
+          sfx_come_again.play();
+        }, id2);
+      }
     }
   }
 })
@@ -76,12 +99,12 @@ const _sfx_thank_you_for: Howl = new Howl({
 const sfx_gameover_arr: readonly Howl[] = howl_loader("/audio/voice/game_over_%i.mp3", 4)
   .concat(howl_loader("/audio/voice/skill_issue.mp3"))
   .concat(howl_loader("/audio/voice/try_again.mp3"))
-  .concat([_sfx_thank_you_for])
+  .concat([_sfx_thank_you_for_playing])
 ;
 
 const sfx_item_arr: readonly Howl[] = howl_loader("/audio/voice/item_%i.mp3", 3);
 
-const sfx_gamba_arr: readonly Howl[] = howl_loader("/audio/voice/lets_gambling_%i.mp3",7);
+const sfx_gamba_arr: readonly Howl[] = howl_loader("/audio/voice/lets_gambling_%i.mp3",8);
 
 const sfx_no_arr: readonly Howl[] = howl_loader("/audio/voice/no_%i.mp3", 3);
 
@@ -89,4 +112,47 @@ const sfx_sell_arr: readonly Howl[] = howl_loader("/audio/voice/sell_%i.mp3", 2)
 
 const sfx_yes_arr: readonly Howl[] = howl_loader("/audio/voice/yes_%i.mp3",5);
 
+const sfx_no_refund_arr: readonly Howl[] = howl_loader("/audio/voice/no_refund_%i.mp3", 5);
 
+const sfx_buy_arr: readonly Howl[] = howl_loader("/audio/voice/buy_%i.mp3", 3);
+
+const sfx_box_arr: readonly Howl[] = howl_loader("/audio/voice/box_%i.mp3", 8);
+
+const sfx_good_luck: Howl = new Howl({
+  src: ["/audio/voice/good_luck.mp3"]
+});
+
+const sfx_come_again: Howl = new Howl({
+  src: ["/audio/voice/come_again.mp3"]
+});
+
+const sfx_thank_you_for_buying: Howl = new Howl({
+  src: ["/audio/voice/thank_you_for.mp3"],
+  onend: soundId => {
+    if (Math.random() >= 0.5){
+      const h1 = _random_itm(sfx_buy_arr)
+      const id1 = h1.play();
+      if (Math.random() >= 0.5){
+        h1.once('end', () => {
+          sfx_come_again.play();
+        }, id1);
+      }
+    } else {
+      const [h2, id2] = Asset_db.sfx_money();
+      if (Math.random() >= 0.5){
+        h2.once('end', () => {
+          sfx_come_again.play();
+        }, id2);
+      }
+    }
+  }
+});
+
+const sfx_purchase_arr: readonly Howl[] = sfx_buy_arr
+  .concat(sfx_box_arr)
+  .concat(sfx_no_refund_arr)
+  .concat([sfx_thank_you_for_buying, sfx_good_luck]);
+
+export {
+  Asset_db
+}
